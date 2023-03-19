640bbfedb8a55c2971456696645ffd8a1f449d78
Kamil <kgoralczyk@jash.pl>
2023-03-19 11:33:20 +0100

    Proper flashBag message display after toggle; Added index.php files

--- src/Controller/Admin/CategorySpecialController.php
+++ src/Controller/Admin/CategorySpecialController.php
@@ -8,21 +8,27 @@ use Kamil\CategorySpecial\Domain\Reviewer\Exception\CategorySpecialException;
 use Kamil\CategorySpecial\Domain\Reviewer\Exception\CannotToggleCategorySpecialStatusException;
 use PrestaShopBundle\Controller\Admin\FrameworkBundleAdminController;
 use PrestaShop\PrestaShop\Core\Domain\Category\Exception\CategoryException;
-use Configuration;
 
 class CategorySpecialController extends FrameworkBundleAdminController
 {
     public function toggleCategorySpecialAction($categoryId)
     {
         try {
-            $this->getCommandBus()->handle(new ToggleCategorySpecialCommand((int) $categoryId));
+            $this->getCommandBus()->handle(new ToggleCategorySpecialCommand((int)$categoryId));
 
-            $this->addFlash('success', $this->trans('Successful update.', 'Admin.Notifications.Success'));
+            $response = [
+                'status' => true,
+                'message' => $this->trans('The special category status had been updated.',
+                    'Modules.Categoryspecial.Categoryspecialcontroller'),
+            ];
         } catch (CategorySpecialException $e) {
-            $this->addFlash('error', $this->getErrorMessageForException($e, $this->getErrorMessageMapping()));
+            $response = [
+                'status' => false,
+                'message' => $this->getErrorMessageForException($e, $this->getErrorMessageMapping()),
+            ];
         }
 
-        return $this->redirectToRoute('admin_categories_index');
+        return $this->json($response);
     }
 
     private function getErrorMessageMapping()
